67b484df6f10e38d01208386f4771d3207b657a3
Luan Nguyen <mrb1218@yahoo.com>
2016-10-18 14:27:47 -0600

    LALA

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
 end
+
+def current_user
+    User.find(session[:current_user_id])
+end
+    
